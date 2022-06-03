95efbf096445085c415c53e531a50956322e8859
Ian Dinwoodie <dinwoodieian@gmail.com>
2022-06-03 12:53:09 -0400

    Isolated cli logic.

--- src/pdfpop/cli.py
+++ src/pdfpop/cli.py
@@ -1,27 +1,16 @@
 """CLI module for pdfpop."""
 import sys
 import pathlib
-
 import argparse
-import pdfrw
-import pandas as pd
 
 from pdfpop import __version__
-
-
-ANNOT_KEY = "/Annots"
-ANNOT_FIELD_KEY = "/T"
-ANNOT_VAL_KEY = "/V"
-ANNOT_RECT_KEY = "/Rect"
-SUBTYPE_KEY = "/Subtype"
-WIDGET_SUBTYPE_KEY = "/Widget"
+from pdfpop.main import pdfpop
 
 
 def main():
     """Main pdfpop function."""
     args = parse_cli()
-    data = build_data_dict(args.excel)
-    fill_pdf(args.pdf, args.out, data)
+    pdfpop(args.pdf, args.excel, args.out)
 
 
 def parse_cli():
@@ -46,37 +35,6 @@ def version_msg():
     location = pathlib.Path(__file__).resolve().parents[1]
     message = f"pdfpop {__version__} from {location} (Python {python_version})"
     return message
-
-
-def build_data_dict(excel_file_path):
-    """Build a dictionary of data to be used to populate the PDF."""
-    df = pd.read_excel(excel_file_path, header=None, index_col=0)
-    df = df.where(pd.notnull(df), None)
-    return df.to_dict()[1]
-
-
-def fill_pdf(input_pdf_path, output_pdf_path, data):
-    """Fill the PDF with data and output at the specified path."""
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    for page in template_pdf.pages:
-        annotations = page[ANNOT_KEY]
-        for annotation in annotations:
-            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                if annotation[ANNOT_FIELD_KEY]:
-                    key = annotation[ANNOT_FIELD_KEY][1:-1]
-                    if key in data.keys():
-                        if isinstance(data[key], bool):
-                            if data[key] is True:
-                                annotation.update(
-                                    pdfrw.PdfDict(AS=pdfrw.PdfName("Yes"))
-                                )
-                        else:
-                            annotation.update(pdfrw.PdfDict(V="{}".format(data[key])))
-                            annotation.update(pdfrw.PdfDict(AP=""))
-    template_pdf.Root.AcroForm.update(
-        pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject("true"))
-    )
-    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
 
 
 if __name__ == "__main__":
---
+++ src/pdfpop/main.py
@@ -0,0 +1,47 @@
+"""Main entry point for the `pdfpop` command."""
+import pandas as pd
+import pdfrw
+
+ANNOT_KEY = "/Annots"
+ANNOT_FIELD_KEY = "/T"
+ANNOT_VAL_KEY = "/V"
+ANNOT_RECT_KEY = "/Rect"
+SUBTYPE_KEY = "/Subtype"
+WIDGET_SUBTYPE_KEY = "/Widget"
+
+
+def pdfpop(input_pdf_path, input_excel_path, output_pdf_path):
+    """Populate the PDF with data from an Excel file."""
+    data = build_data_dict(input_excel_path)
+    fill_pdf(input_pdf_path, output_pdf_path, data)
+
+
+def build_data_dict(input_excel_path):
+    """Build a dictionary of data to be used to populate the PDF."""
+    df = pd.read_excel(input_excel_path, header=None, index_col=0)
+    df = df.where(pd.notnull(df), None)
+    return df.to_dict()[1]
+
+
+def fill_pdf(input_pdf_path, output_pdf_path, data):
+    """Fill the PDF with data and output at the specified path."""
+    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    for page in template_pdf.pages:
+        annotations = page[ANNOT_KEY]
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                if annotation[ANNOT_FIELD_KEY]:
+                    key = annotation[ANNOT_FIELD_KEY][1:-1]
+                    if key in data.keys():
+                        if isinstance(data[key], bool):
+                            if data[key] is True:
+                                annotation.update(
+                                    pdfrw.PdfDict(AS=pdfrw.PdfName("Yes"))
+                                )
+                        else:
+                            annotation.update(pdfrw.PdfDict(V="{}".format(data[key])))
+                            annotation.update(pdfrw.PdfDict(AP=""))
+    template_pdf.Root.AcroForm.update(
+        pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject("true"))
+    )
+    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
