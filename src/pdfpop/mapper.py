115d5728cb930aca15de58b49a21f51caa8351f0
Ian Dinwoodie <dinwoodieian@gmail.com>
2022-06-20 15:57:41 -0400

    Improved mapper tests.

--- src/pdfpop/mapper.py
+++ src/pdfpop/mapper.py
@@ -1,28 +1,34 @@
 """Module for pdfpop mapping."""
 
 
+def _wrap_logic(logic: str) -> str:
+    """Returns a function wrapping the given logic."""
+    if "return" in logic:
+        return f"def fn(data):\n    {logic}\nrv = fn(data)\n"
+    return f"rv = {logic}\n"
+
+
 def map_fields(fields: dict[str, str], data: dict[str, str]) -> dict[str, str]:
     """Build a dictionary of field keys to interpreted values."""
-
-    def get_code_template() -> str:
-        return "def fn(data):\n    %s\nrv = fn(data)\n"
-
     mapped_fields = {}
     print("\nEvent Log:")
     ignore_list = []
     for key, value in fields.items():
         if value is None:
             ignore_list.append(key)
+            continue
         elif value in data:
-            print(f'Set field "{key}" to "{data[value]}"')
             mapped_fields[key] = data[value]
+        elif isinstance(value, int) or isinstance(value, float):
+            mapped_fields[key] = value
         else:
             global_env = {}
             local_env = {"data": data}
-            full_expr = get_code_template() % value
-            exec(full_expr, global_env, local_env)
-            print(f'Set field "{key}" to "{local_env["rv"]}"')
-            mapped_fields[key] = local_env["rv"]
+            expr = _wrap_logic(value)
+            exec(expr, global_env, local_env)
+            rv = local_env["rv"]
+            mapped_fields[key] = rv if rv is not None else value
+        print(f'Set field "{key}" to "{mapped_fields[key]}"')
     for key in ignore_list:
         print(f'Ignored field "{key}"')
     return mapped_fields
--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -4,51 +4,66 @@ import pytest
 import pdfpop.mapper
 
 
-def test_mapper_map_fields_empty_args_returns_empty_dict():
+def test_map_fields_empty_args_returns_empty_dict():
     """Tests that empty args result in an empty dict."""
     assert pdfpop.mapper.map_fields({}, {}) == {}
 
 
-def test_mapper_map_fields_null_value_ignoed():
+def test_map_fields_ignore_null_mapped_keys():
     """Tests that fields mapped to None are ignored."""
     fields = {"a1": None, "b1": "return 456"}
     mapped_fields = pdfpop.mapper.map_fields(fields, {})
     assert mapped_fields == {"b1": 456}
 
 
-def test_mapper_map_fields_column_as_value():
-    """Tests that column as value works."""
+def test_map_fields_value_mapped_keys():
+    """Tests that keys mapped to values are supported."""
+    fields = {"a1": 123, "b1": "456", "c1": "'789'"}
+    mapped_fields = pdfpop.mapper.map_fields(fields, {})
+    assert mapped_fields == {"a1": 123, "b1": 456, "c1": "789"}
+
+
+def test_map_fields_col_mapped_keys():
+    """Tests that keys mapped to columns are supported."""
     fields = {"a1": "a2", "b1": "b2"}
     data = {"a2": 123, "b2": "456"}
     mapped_fields = pdfpop.mapper.map_fields(fields, data)
     assert mapped_fields == {"a1": 123, "b1": "456"}
 
 
-def test_mapper_map_fields_function_as_value():
-    """Tests that functions as values works."""
+def test_map_fields_code_mapped_keys():
+    """Tests that keys mapped to code are supported."""
+    fields = {"a1": "122+1", "b1": "'45' + '6'"}
+    mapped_fields = pdfpop.mapper.map_fields(fields, {})
+    assert mapped_fields == {"a1": 123, "b1": "456"}
+
+
+def test_map_fields_rv_mapped_keys():
+    """Tests that keys mapped to return values are supported."""
     fields = {"a1": "return 122+1", "b1": 'return "456"'}
     mapped_fields = pdfpop.mapper.map_fields(fields, {})
     assert mapped_fields == {"a1": 123, "b1": "456"}
 
 
-def test_mapper_map_fields_mixed_type_values():
-    """Tests that a mix of functions and columns as values works."""
-    fields = {"a1": "return 122+1", "b1": "b2"}
-    data = {"b2": "456"}
+def test_map_fields_mixed_mapping_types():
+    """Tests that mixed mapping types across independent keys are supported."""
+    fields = {"a1": "122+1", "b1": "b2", "c1": "return '789'"}
+    data = {"b2": 456}
     mapped_fields = pdfpop.mapper.map_fields(fields, data)
-    assert mapped_fields == {"a1": 123, "b1": "456"}
+    assert mapped_fields == {"a1": 123, "b1": 456, "c1": "789"}
 
 
-def test_mapper_map_fields_invalid_column_errors():
-    """Tests that an invalid column results in a name error."""
-    fields = {"a1": "a2"}
-    with pytest.raises(NameError):
-        mapped_fields = pdfpop.mapper.map_fields(fields, {})
-
-
-def test_mapper_map_fields_function_data_access():
-    """Tests that functions can access data."""
-    fields = {"a1": "return data['a2']", "b1": "return data['b2']"}
+def test_map_fields_data_access():
+    """Tests that mappings have data access."""
+    fields = {"a1": "data['a2']", "b1": "return data['b2']"}
     data = {"a2": 123, "b2": "456"}
     mapped_fields = pdfpop.mapper.map_fields(fields, data)
     assert mapped_fields == {"a1": 123, "b1": "456"}
+
+
+def test_map_fields_data_access_error():
+    """Tests that mappings have data access."""
+    fields = {"a1": "data['a2']", "b1": "return data['b2']"}
+    data = {"a2": 123}
+    with pytest.raises(KeyError):
+        pdfpop.mapper.map_fields(fields, data)
