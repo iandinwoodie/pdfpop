2417f353c1de52e37e4a3396f76470eb0566b1bc
Ian Dinwoodie <dinwoodieian@gmail.com>
2022-06-21 23:33:24 -0400

    Added test for invocation.

---
+++ tests/test_pdfpop_invocation.py
@@ -0,0 +1,21 @@
+"""
+Tests to make sure that pdfpop can be called from the cli without using the
+entry point set up for the package.
+"""
+
+import os
+import subprocess
+import sys
+
+import pytest
+
+
+def test_should_invoke_main(monkeypatch):
+    """Should produce pdfpop help message and exit with 0 code."""
+    monkeypatch.setenv("PYTHONPATH", ".")
+
+    result = subprocess.run(
+        [sys.executable, "-m", "pdfpop", "-h"], capture_output=True
+    )
+    assert result.returncode == 0
+    assert result.stdout.decode("utf-8").startswith("Usage: pdfpop")
